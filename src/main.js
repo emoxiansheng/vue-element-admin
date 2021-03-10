4460e6aa3f2e35dda816bff71f674e423c2c610e
txt <1327041751@qq.com>
2021-03-11 01:15:29 +0800

    1.初始化配置

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui' // elemenUI
+import 'element-ui/lib/theme-chalk/index.css' // elementUI主题样式
+import 'font-awesome/css/font-awesome.min.css' // 奥森图标库
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI) // 全局注册
 
 /* eslint-disable no-new */
 new Vue({
