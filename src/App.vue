65d442dd5ca35d8d6e3b079dc4f297fa5fe26621
1327041751 <1327041751@qq.com>
2021-03-13 20:09:55 +0800

    1.修复提交冲突

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-=======
-    <img src="./assets/logo.png">
->>>>>>> 4460e6aa3f2e35dda816bff71f674e423c2c610e
     <router-view/>
   </div>
 </template>
@@ -15,19 +11,9 @@ export default {
 </script>
 
 <style>
-<<<<<<< HEAD
 @import "./assets/css/main.css";
 @import "./assets/css/color-dark.css";     /*深色主题*/
 body  {
   margin: 0;
-=======
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
->>>>>>> 4460e6aa3f2e35dda816bff71f674e423c2c610e
 }
 </style>
